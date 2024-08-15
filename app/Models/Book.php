3e83b6b41a5c5e467362b8708b50ad6a04c82f2a
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-08-15 18:46:58 +0530

    modify Book model, creating one to many relationship, which basically tells us that the book can have many reviews

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Facades\Cache;
 
 class Book extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
 }
