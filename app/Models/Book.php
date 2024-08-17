341f1ade1ad5e13dbdef8b7d3db537a0c0446d93
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-08-17 17:12:47 +0530

    modify Book model, defining local query scope for the most popular and highest rated books. And defining scopes for getting books with recent reviews using arrow function.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -17,4 +17,49 @@ class Book extends Model
         return $this->hasMany(Review::class);
     }
 
+    public function scopeTitle(Builder $query, $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query-> withCount([
+            'reviews' => fn(Builder $q) => $this -> dateRangeFilter($q, $from, $to)
+        ]);
+    }
+
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this -> dateRangeFilter($q, $from, $to)
+        ], 'rating');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query-> withReviewsCount() -> orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvgRating() -> orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
+    {
+        return $query-> having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query-> where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query-> where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query-> whereBetween('created_at', [$from, $to]);
+        }
+    }
+
 }
