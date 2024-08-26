22abc46483100d9b7771b50b251eb4c53724bbdb
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-08-26 12:28:10 +0530

    modify Review model, add a protected static function called booted, to perform cache invalidation.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -17,5 +17,9 @@ class Review extends Model
         return $this->belongsTo(Book::class);
     }
 
-    
+    protected static function booted() 
+    {
+        static::updated(fn(Review $review) => Cache::forget('book:'. $review->book_id));
+        static::deleted(fn(Review $review) => Cache::forget('book:'. $review->book_id));
+    }
 }
