a4e7cec3e817df54653f0013b5bb5f75dfa63d6f
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-08-26 11:10:22 +0530

    modify BookController, add caching mechanism to reduce the strain on the infrastructure because of running database query.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+
 
 class BookController extends Controller
 {
@@ -30,9 +30,9 @@ class BookController extends Controller
 
          };
 
-        
-        $books = 
-            $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title; 
+        $books = cache()-> remember($cacheKey, 3600, fn() => $books->get());
+            
 
         return view('books.index', ['books' => $books]);
     }
