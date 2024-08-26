c37f4e0783b48a36de8726cda47b5550b0ebd0c5
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-08-26 12:54:49 +0530

    modify BookController, to display the rating and review count on the single book.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -56,14 +56,18 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
-        $cacheKey = 'book:' . $book->id;
+        $cacheKey = 'book:' . $id;
 
-        $book =  Cache::remember($cacheKey, 3600, fn() => $book->load([
+        $book =  Cache::remember(
+            $cacheKey, 
+            3600, 
+            fn() => Book::with([
             'reviews'=> fn($query) => $query->latest()
-        ]));
-        
+            ])->withAvgRating()-> withReviewsCount()-> findOrFail($id)
+        );
+
         return view( 'books.show', [ 'book' => $book ] );
     }
 
