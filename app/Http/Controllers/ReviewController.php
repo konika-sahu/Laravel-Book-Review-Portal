b57aaf629d4093ca463a32cadcaf2165b83692a8
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-09-01 00:00:40 +0530

    modify ReviewController, add data validation and create a new book review and also redirect it to show the book review page in store action of ReviewController.

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -31,7 +31,14 @@ class ReviewController extends Controller
      */
     public function store(Request $request, Book $book)
     {
-        //
+        $data = $request->validate([
+            'review' => 'required|min:15',
+            'rating' => 'required|min:1|max:5|integer'
+        ]);
+
+        $book->reviews()->create($data);
+
+        return redirect()->route('books.show', $book);
     }
 
     /**
